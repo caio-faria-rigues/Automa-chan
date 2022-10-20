8e720e5ead73db7367a1d9f6500aa6516daf82f6
Caio Rigues <105236708+caio-faria-rigues@users.noreply.github.com>
2022-10-20 17:36:02 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import voice_functions as voice_function
+from voice_functions import reco
+import threading
+import speech_recognition as sr
+from time import sleep
+
+r = sr.Recognizer()
+voice = ''
+
+def hear():
+    global voice
+    print("ready!")
+    with sr.Microphone() as source:
+        while voice != 'sair':
+            try:
+                audio = r.listen(source)
+
+                voice = r.recognize_google(audio, language='pt')
+                print(f">>{voice}")
+                voice_function.Listener(voice)
+
+                automa_write.config(text=voice)
+                # automa_write.after(5000, hear)
+            except:
+                pass
+
+
+
+
+
+root = Tk()
+root.title("Automa-chan")
+root.minsize(1280, 720)
+root.maxsize(1280, 720)
+background_image = PhotoImage(
+    file=r'C:\Users\Cliente\PycharmProjects\guppe\Projetos\Automa-chan alpha 2.0\src\background1.png')
+canvas1 = Canvas(root)
+canvas1.pack(fill="both", expand = True)
+canvas1.create_image(0, 0, image=background_image, anchor="nw")
+
+automa_write = Label(root, text='', font=("Retro Gaming", 12), background="#e9d0d6")
+automa_write.place(y=550, x=18)
+
+automa_hear = Text(root, font=("Retro Gaming", 12), background="#e9d0d6", height=22, width=25)
+automa_hear.place(y=12, x=968)
+
+threading.Thread(target=hear).start()
+
+
+def recognizer():
+    global automa_write
+    global voice
+    while voice != "sair":
+        # automa_write.config(text=voice)
+        voicestr = ''
+        voice_function.Listener(voice)
+        voicestr = voice if voicestr != voice else ''
+        response = reco(voice)
+        print(response) if voice is not None else print('')
+        automa_write.config(text=response)
+        automa_hear.insert(END, voicestr + '\n') if voicestr != '' else None
+        voicestr = ''
+        sleep(1)
+
+
+threading.Thread(target=recognizer).start()
+
+root.mainloop()
---
+++ voice_functions.py
@@ -0,0 +1,233 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+import speech_recognition as sr
+import webbrowser
+from time import sleep
+
+""""""
+r = sr.Recognizer()
+
+voice = ''
+
+maiusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U',
+              'V', 'W', 'X', 'Y', 'Z']
+
+
+def city(cidade):
+    for resultado in search(f'"{cidade}" accuweather', stop=1):
+        citylink = resultado
+
+    return citylink
+
+
+def temperatura(citylink):
+    url = str(citylink)
+    headers = {
+                "User-Agent":
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"}
+
+    site = requests.get(url, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    citytemp = soup.find('div', class_='temp').get_text()
+
+    return citytemp
+
+
+def pesquisa_musica(musica):
+    for resultado in search(f'"{musica}" youtube', stop=1):
+        music = resultado
+
+    return music
+
+
+def separa_maiusculo(frase):
+    cidade = []
+    lista = frase.split(" ")
+    for palavra in lista:
+        if palavra[0] in maiusculas:
+            cidade.append(palavra)
+    cidadestr = " ".join(cidade)
+    return cidadestr
+
+""""""
+
+
+class Voice:
+    def voicereco(self):
+        with sr.Microphone() as source:
+            while voice != 'sair':
+                try:
+                    audio = self.listen(source)
+
+                    voice = self.recognize_google(audio, language='pt')
+                    print(f">>{voice}")
+                    if "ei" in voice:
+                        if "Bom dia" in str(voice):
+                            print("Olá")
+                        elif "temperatura" in voice:
+                            cidade = separa_maiusculo(str(voice))
+                            print(temperatura(city(cidade)))
+                        elif "graus" in voice:
+                            cidade = separa_maiusculo(str(voice))
+                            print(temperatura(city(cidade)))
+                        elif "Chainsaw Man" in str(voice):
+                            print("Fujima God simplesmente")
+                        else:
+                            pass
+                    if "Ei" in voice:
+                        if "Bom dia" in str(voice):
+                            print("Olá")
+                        elif "temperatura" in voice:
+                            cidade = separa_maiusculo(str(voice))
+                            print(temperatura(city(cidade)))
+                        elif "graus" in voice:
+                            cidade = separa_maiusculo(str(voice))
+                            print(temperatura(city(cidade)))
+                        elif "Chainsaw Man" in str(voice):
+                            print("Fujima God simplesmente")
+                        elif "ouvir" in str(voice):
+                            musica = str(voice)
+                            url = pesquisa_musica(musica)
+                            webbrowser.open(url)
+                        else:
+                            pass
+                except:
+                    pass
+
+
+class Listener:
+    def __init__(self, voz):
+        user_voice = voz
+        if "ei" in user_voice:
+            if "Bom dia" in str(user_voice):
+                print("Olá")
+            elif "temperatura" in user_voice:
+                cidade = separa_maiusculo(str(user_voice))
+                print(temperatura(city(cidade)))
+            elif "graus" in user_voice:
+                cidade = separa_maiusculo(str(user_voice))
+                print(temperatura(city(cidade)))
+            elif "Chainsaw Man" in str(user_voice):
+                print("Fujima God simplesmente")
+            else:
+                pass
+        if "Ei" in user_voice:
+            if "Bom dia" in str(user_voice):
+                print("Olá")
+            elif "temperatura" in user_voice:
+                cidade = separa_maiusculo(str(user_voice))
+                print(temperatura(city(cidade)))
+            elif "graus" in user_voice:
+                cidade = separa_maiusculo(str(user_voice))
+                print(temperatura(city(cidade)))
+            elif "Chainsaw Man" in str(user_voice):
+                print("Fujima God simplesmente")
+            elif "ouvir" in str(user_voice):
+                musica = str(user_voice)
+                url = pesquisa_musica(musica)
+                webbrowser.open(url)
+            else:
+                pass
+
+    def city(cidade):
+        for resultado in search(f'"{cidade}" accuweather', stop=1):
+            citylink = resultado
+
+        return citylink
+
+    def temperatura(citylink):
+        url = str(citylink)
+        headers = {
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"}
+
+        site = requests.get(url, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+        citytemp = soup.find('div', class_='temp').get_text()
+
+        return citytemp
+
+    def pesquisa_musica(musica):
+        for resultado in search(f'"{musica}" youtube', stop=1):
+            music = resultado
+
+        return music
+
+    def separa_maiusculo(frase):
+        cidade = []
+        lista = frase.split(" ")
+        for palavra in lista:
+            if palavra[0] in maiusculas:
+                cidade.append(palavra)
+        cidadestr = " ".join(cidade)
+        return cidadestr
+
+
+def reco(voz):
+    def city(cidade):
+        for resultado in search(f'"{cidade}" accuweather', stop=1):
+            citylink = resultado
+
+        return citylink
+
+    def temperatura(citylink):
+        url = str(citylink)
+        headers = {
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"}
+
+        site = requests.get(url, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+        citytemp = soup.find('div', class_='temp').get_text()
+
+        return citytemp
+
+    def pesquisa_musica(musica):
+        for resultado in search(f'"{musica}" youtube', stop=1):
+            music = resultado
+
+        return music
+
+    def separa_maiusculo(frase):
+        cidade = []
+        lista = frase.split(" ")
+        for palavra in lista:
+            if palavra[0] in maiusculas:
+                cidade.append(palavra)
+        cidadestr = " ".join(cidade)
+        return cidadestr
+
+    user_voice = voz
+    if "ei" in user_voice:
+        if "Bom dia" in str(user_voice):
+            print("Olá")
+        elif "temperatura" in user_voice:
+            cidade = separa_maiusculo(str(user_voice))
+            print(temperatura(city(cidade)))
+            return temperatura((city(cidade)))
+        elif "graus" in user_voice:
+            cidade = separa_maiusculo(str(user_voice))
+            print(temperatura(city(cidade)))
+        elif "Chainsaw Man" in str(user_voice):
+            print("Fujima God simplesmente")
+        else:
+            pass
+    if "Ei" in user_voice:
+        if "Bom dia" in str(user_voice):
+            print("Olá")
+        elif "temperatura" in user_voice:
+            cidade = separa_maiusculo(str(user_voice))
+            print(temperatura(city(cidade)))
+        elif "graus" in user_voice:
+            cidade = separa_maiusculo(str(user_voice))
+            print(temperatura(city(cidade)))
+        elif "Chainsaw Man" in str(user_voice):
+            print("Fujima God simplesmente")
+        elif "ouvir" in str(user_voice):
+            musica = str(user_voice)
+            url = pesquisa_musica(musica)
+            webbrowser.open(url)
+        else:
+            pass
+
